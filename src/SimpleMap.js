27c55352032aed2e4e857eba64e5264c3d681a75
cristianbgp <cristian.granda.pastor@gmail.com>
2019-06-09 21:48:18 -0500

    Fix zoom

--- src/SimpleMap.js
+++ src/SimpleMap.js
@@ -3,6 +3,9 @@ import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 function SimpleMap() {
   const [position, setPosition] = React.useState([0, 0]);
+  const [zoomState, setZoomState] = React.useState(16);
+
+  const mapRef = React.useRef();
 
   React.useEffect(() => {
     const watchID = navigator.geolocation.watchPosition(pos => {
@@ -13,8 +16,17 @@ function SimpleMap() {
     };
   }, [setPosition]);
 
+  const handleOnZoom = map => {
+    setZoomState(mapRef.current.leafletElement.getZoom());
+  };
+
   const map = (
-    <Map center={position} zoom={13}>
+    <Map
+      center={position}
+      zoom={zoomState}
+      ref={mapRef}
+      onzoomend={handleOnZoom}
+    >
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
